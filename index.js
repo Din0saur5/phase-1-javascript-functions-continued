c2f4b4a820ad21580b86df6ce192af361b9199f7
Dan Hackworth <danielhackworth515@gmail.com>
2023-10-02 18:26:56 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+let saturdayFun = (event = 'roller-skate') => {
+        return `This Saturday, I want to ${event}!`;
+
+}
+saturdayFun();
+
+function mondayWork(event = 'go to the office'){
+    return `This Monday, I will ${event}.`;
+}
+
+
+function statement(msg){
+    return msg;
+}
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  const encouragingPromptFunction = wrapAdjective();
+  
